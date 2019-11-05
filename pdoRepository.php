9a19e88a525b31f5a26dab5adf5dabda1f119de1
December69 <45872580+December69@users.noreply.github.com>
2019-11-05 12:03:25 +0100

    Add files via upload

---
+++ pdoRepository.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Classe de gestion de base de notre base de donées
+*/
+
+abstract class pdoRepository{
+    const USERNAME="id8787072_mike";
+    const PASSWORD="MikeMike";
+    const HOST="localhost";
+    const DB="id8787072_formulakids";
+
+    private function getConnection(){
+        $username = self::USERNAME;
+        $password = self::PASSWORD;
+        $host = self::HOST;
+        $db = self::DB;
+        $connection = new PDO("mysql:dbname=$db;host=$host", $username, $password,
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        //le format utf8 est très important, notamment pour JSON !
+        return $connection;
+    }
+
+    /*************************************
+     * Méthode d'excution d'un SELECT
+     * $sql : requête sql (chaine de caractere)
+     *$arg : tableau à deux dimensions des arguments de la requête sous la forme $arg[i] = [valeur, PDO::PDO::PARAM]
+     */
+    protected function queryList($sql, $args = null){
+        $connection = $this->getConnection();
+        $stmt = $connection->prepare($sql);
+        if($args != null) {
+            $size = count($args);
+            for($i = 0; $i<$size ; $i++){
+                $stmt->bindValue($i+1, $args[$i][0], $args[$i][1]);
+            }
+        }
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /*************************************
+     * Méthode d'excution d'une NON-QUERY (UPDATE, DELETE)
+     *$sql : requête sql (chaine de caractere)
+     *$arg : tableau à deux dimensions des arguments de la requête sous la forme $arg[i] = [valeur, PDO::PDO::PARAM]
+     */
+    protected function execute($sql, $args=null){
+        $connection = $this->getConnection();
+        $stmt = $connection->prepare($sql);
+        if($args != null) {
+            $size = count($args);
+            for($i = 0; $i<$size ; $i++){
+                $stmt->bindValue($i+1, $args[$i][0], $args[$i][1]);
+            }
+        }
+        $stmt->execute();
+        $count = $stmt->rowCount();
+        return $count != 0 ? $count : $stmt->errorInfo();
+    }
+}
+
+?>
